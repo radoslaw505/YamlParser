1ddfb1cb1a363b7bc3f63a5181e739776392027f
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2021-02-13 22:32:16 +0100

    commit

--- src/main/java/org/binge/radoslaw/repositories/DatabaseConfig.java
+++ src/main/java/org/binge/radoslaw/repositories/DatabaseConfig.java
@@ -1,6 +1,8 @@
 package org.binge.radoslaw.repositories;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DatabaseConfig {
 
@@ -9,6 +11,25 @@ public class DatabaseConfig {
     private List<MainDatabase> mainDatabase;
     private List<SupportDatabase> supportDatabase;
 
+
+    public int getMainDatabaseServerId() {
+        return mainDatabase.get(0).getServerId();
+    }
+
+    public String getMainDatabaseUrl() {
+        return mainDatabase.get(0).getUrl();
+    }
+
+    public String getSupportDatabaseUrlById(int i) {
+        Map<Integer, String> supportDatabaseMap = supportDatabase.stream()
+                .collect(Collectors.toMap(SupportDatabase::getServerId, SupportDatabase::getUrl));
+        String url =  supportDatabaseMap.get(i);
+        if (url == null) {
+            throw new ArrayIndexOutOfBoundsException("No value found for requested key: " + i);
+        } else {
+            return url;
+        }
+    }
 
     public String getDatabaseUser() {
         return databaseUser;
@@ -20,14 +41,6 @@ public class DatabaseConfig {
 
     public List<MainDatabase> getMainDatabase() {
         return mainDatabase;
-    }
-
-    public int getMainDatabaseServerId() {
-        return mainDatabase.get(0).getServerId();
-    }
-
-    public String getMainDatabaseUrl() {
-        return mainDatabase.get(0).getUrl();
     }
 
     public List<SupportDatabase> getSupportDatabase() {
--- src/test/java/org/binge/radoslaw/YamlParserTest.java
+++ src/test/java/org/binge/radoslaw/YamlParserTest.java
@@ -32,4 +32,20 @@ class YamlParserTest {
         assertEquals("jdbc:mysql://192.168.1.10/MAIN", dbConfig.getMainDatabaseUrl());
     }
 
+    @Test
+    public void testParsedSupportDatabase() {
+        assertEquals("jdbc:mysql://192.168.1.12/SUP1", dbConfig.getSupportDatabaseUrlById(2));
+        assertEquals("jdbc:mysql://192.168.3.20/SUP2", dbConfig.getSupportDatabaseUrlById(3));
+        assertEquals("jdbc:mysql://192.168.3.22/SUP3", dbConfig.getSupportDatabaseUrlById(4));
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            dbConfig.getSupportDatabaseUrlById(666);
+        });
+
+        String expectedMessage = "No value found for requested key: 666";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
